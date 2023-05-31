0e14dc70ba39411bf5eaf2d4b0654b35f7be9e94
Sanket <sanketughadmathe@gmail.com>
2023-05-31 18:42:24 +0530

    Code changes

--- 02. Project Building a Restful API for Similarity Check using NLP/app.py
+++ 02. Project Building a Restful API for Similarity Check using NLP/app.py
@@ -13,7 +13,7 @@ api = Api(app)
 
 # client = MongoClient("mogodb://db:27017")
 client = MongoClient("mongodb://localhost:27017")
-db = client.SentencesDatabase
+db = client.SimilarityDB
 users = db["Users"]
 
 
@@ -46,7 +46,7 @@ class Register(Resource):
         # Getting the json data from the request
         postedData = request.get_json()
 
-        # Get the data
+        # Getting the data
         userName = postedData["Username"]
         password = postedData["Password"]
 
@@ -70,12 +70,8 @@ class Register(Resource):
             record = {
                 "Username": userName,
                 "Password": hashedPassword.decode('utf-8'),
-                "Sentence": "",
                 "Tokens": 6
             }
-
-            # dumpVar = json.dumps(record)
-            # loadVar = json.loads(dumpVar)
 
             users.insert_one(record)
             retJson = {
@@ -85,14 +81,16 @@ class Register(Resource):
             return jsonify(retJson)
 
 
-class Store(Resource):
+class Detect(Resource):
     def post(self):
         # Getting the json data from the request
         postedData = request.get_json()
 
+        # Getting the data
         userName = postedData["Username"]
         password = postedData["Password"]
-        sentence = postedData["Sentence"]
+        text1 = postedData["text1"]
+        text2 = postedData["text2"]
 
         # Verify the username
         if not userExist(userName):
@@ -116,76 +114,7 @@ class Store(Resource):
         numTokens = countTokens(userName)
         if numTokens <= 0:
             retJson = {
-                "status": 301,
-                "message": "You don't have enough tokens"
+                "status": 303,
+                "message": "You don't have enough tokens, please refill!"
             }
             return jsonify(retJson)
-
-        # store the sentence
-        users.update_one({
-            "Username": userName
-        }, {
-            "$set": {
-                "Sentence": sentence,
-                "Tokens": numTokens-1
-            }
-        })
-        retJson = {
-            "status": 200,
-            "message": "Sentence saved successfully."
-        }
-        return jsonify(retJson)
-
-
-class Get(Resource):
-    def post(self):
-        # Getting the json data from the request
-        postedData = request.get_json()
-
-        userName = postedData["Username"]
-        password = postedData["Password"]
-
-        # Verify the password
-        correctPw = verifyPw(userName, password)
-
-        if not correctPw:
-            retJson = {
-                "status": 302,
-                "message": "You have entered wrong credentials"
-            }
-            return jsonify(retJson)
-
-        # Verify user has enough tokens
-        numTokens = countTokens(userName)
-        if numTokens <= 0:
-            retJson = {
-                "status": 301,
-                "message": "You don't have enough tokens"
-            }
-            return jsonify(retJson)
-
-        # Make the user pay
-        users.update_one({
-            "Username": userName
-        }, {
-            "$set": {
-                "Tokens": numTokens-1
-            }
-        })
-
-        # Return the sentence
-        sentence = users.find_one({"Username": userName})["Sentence"]
-        retJson = {
-            "status": 200,
-            "sentence": sentence
-        }
-        return jsonify(retJson)
-
-
-api.add_resource(Register, '/register')
-api.add_resource(Store, '/store')
-api.add_resource(Get, '/get')
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
